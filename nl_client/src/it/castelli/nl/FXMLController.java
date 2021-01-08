ca23c17dde25349c6218bdd2cc3b4d2bd8209ad6
Luca Vaccari <lucavaccari03@gmail.com>
2021-01-08 17:03:49 +0100

    Added FXML for client

---
+++ nl_client/src/it/castelli/nl/FXMLController.java
@@ -0,0 +1,5 @@
+package it.castelli.nl;
+
+public class FXMLController
+{
+}
--- nl_client/src/it/castelli/nl/NLClient.java
+++ nl_client/src/it/castelli/nl/NLClient.java
@@ -1,7 +1,14 @@
 package it.castelli.nl;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
 
 public class NLClient extends Application
 {
@@ -13,9 +20,32 @@ public class NLClient extends Application
 	}
 
 	@Override
-	public void start(Stage primaryStage) throws Exception
+	public void start(Stage primaryStage)
 	{
 		clientThread = new Thread(new ClientReceiver(), "ClientThread");
+
+		FXMLLoader loader = new FXMLLoader();
+		try
+		{
+			loader.setLocation(new File("src/index.fxml").toURI().toURL());
+		}
+		catch (MalformedURLException e)
+		{
+			e.printStackTrace();
+		}
+		Parent root;
+		try
+		{
+			root = loader.load();
+			assert root != null;
+			Scene mainScene = new Scene(root);
+			primaryStage.setScene(mainScene);
+			primaryStage.show();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	@Override
@@ -26,7 +56,8 @@ public class NLClient extends Application
 		{
 			clientThread.join();
 		}
-		catch (InterruptedException e) {
+		catch (InterruptedException e)
+		{
 			e.printStackTrace();
 		}
 		super.stop();
